36f7add40f6cd8bae4013a93a7d427086b6d99a9
Sebastian Bastidas <sbastidasr@gmail.com>
2016-06-05 22:57:18 -0500

    database

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
   "db": {
-		"mongodb": 'mongodb://localhost/caloriecounter',
+		"mongodb": 'mongodb://sebi:sebi@ds015953.mlab.com:15953/caloriecounter',
     "production": "mongodb://username:password@dsXXXXX.mongolab.com:45077/databasename"
 	},
 	"logger": {
@@ -9,3 +9,6 @@ module.exports = {
 	},
   "secret": "asdasdagsdgsdfg"
 };
+
+ //'http://mongodb://saurabh:orange56@ds037283.mongolab.com:37283/notes'
+ //mongodb://<dbuser>:<dbpassword>@ds015953.mlab.com:15953/caloriecounter
